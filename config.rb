2db6f5a06ac200a7e1bd2038a2a0564b64b42c57
Matthew Radcliffe <mradcliffe@softpixel.com>
2014-10-25 15:09:16 -0400

    Add basic angular.js app.

---
+++ config.rb
@@ -0,0 +1,23 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "css/sass"
+images_dir = "css/images"
+javascripts_dir = "js"
+fonts_dir = "css/fonts"
+
+output_style = :compressed
+environment = :production
+
+relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass css/sass scss && rm -rf sass && mv scss sass
